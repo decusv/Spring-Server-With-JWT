66e97a774f4b9fdfbe2332412fdbf7146b57a297
Tomas Riabovas <decusvo@gmail.com>
2024-05-23 19:25:17 +0100

    Adding Javadoc.

--- src/main/java/com/spring_one/webSerrver/security/auth/AuthenticationController.java
+++ src/main/java/com/spring_one/webSerrver/security/auth/AuthenticationController.java
@@ -12,10 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    /**
+     * An AuthenticationService dependency injected via @RequiredArgsConstructor.
+     */
     private final AuthenticationService authenticationService;
 
 
-    // Method returns a AuthenticationResponse object inside the body of the ResponseEntity object.
+    /**
+     *
+     *
+     * @param request - The @RequestBody annotation is used to deserialize the HTTP request into a Java object.
+     * The object's class (RegisterRequest) must contain the fields that the JSON request body would have.
+     *
+     * @return Response Entity is a generic class representing an HTTP response.
+     * It serialises the data from the AuthenticationResponse object such that it can be returned as JSON.
+     */
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register (
             @RequestBody RegisterRequest request
@@ -23,6 +34,15 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
+    /**
+     *
+     *
+     * @param request - The @RequestBody annotation is used to deserialize the HTTP request into a Java object.
+     * The object's class (RegisterRequest) must contain the fields that the JSON request body would have.
+     *
+     * @return Response Entity is a generic class representing an HTTP response.
+     * It serialises the data from the AuthenticationResponse object such that it can be returned as JSON.
+     */
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticateRequest (
             @RequestBody AuthenticationRequest request
